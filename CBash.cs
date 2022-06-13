73f1117eaeb7e9d1e10ec9afe9375f7587052fe8
DeezFurry <105929248+DeezFurry@users.noreply.github.com>
2022-06-13 14:47:11 +0200

    Create CBash.cs

---
+++ CBash.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleProgram
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("This is my first Console Program");
+      Console.WriteLine("Thanks for testing this!")
+    }
+  }
+}
